e76203d242ae3f3e8a391ee52ac9d3ab47737aa1
Vitor Contine <vitorcbs18@gmail.com>
2023-08-08 14:48:10 -0300

    Add project files.

---
+++ Exercicio1/Program.cs
@@ -0,0 +1,21 @@
+﻿Random aleatorio = new Random();
+int numeroSecreto = aleatorio.Next(1, 101);
+
+do
+{
+    Console.WriteLine("Digite um numero entro 1 e 100: ");
+    int chute = int.Parse(Console.ReadLine());
+
+    if (chute == numeroSecreto)
+    {
+        Console.WriteLine("Parabens, voce acertou o numero");
+        break;
+    } else if (chute < numeroSecreto)
+    {
+        Console.WriteLine("O numero e maior!");
+    }
+    else
+    {
+        Console.WriteLine("O numero e menor");
+    } 
+} while(true);  
---
+++ PrimeiroProjeto/Program.cs
@@ -0,0 +1,55 @@
+﻿// Screen Sound
+string mensagemDeBoasVindas = "Boas vindas ao Screen Sound";
+List<string> listaDasBandas = new List<string> { "U2", "The Beatles", "Calypso" };
+
+void ExibirLogo()
+{
+    Console.WriteLine(@"
+
+░██████╗░█████╗░██████╗░███████╗███████╗███╗░░██╗  ░██████╗░█████╗░██╗░░░██╗███╗░░██╗██████╗░
+██╔════╝██╔══██╗██╔══██╗██╔════╝██╔════╝████╗░██║  ██╔════╝██╔══██╗██║░░░██║████╗░██║██╔══██╗
+╚█████╗░██║░░╚═╝██████╔╝█████╗░░█████╗░░██╔██╗██║  ╚█████╗░██║░░██║██║░░░██║██╔██╗██║██║░░██║
+░╚═══██╗██║░░██╗██╔══██╗██╔══╝░░██╔══╝░░██║╚████║  ░╚═══██╗██║░░██║██║░░░██║██║╚████║██║░░██║
+██████╔╝╚█████╔╝██║░░██║███████╗███████╗██║░╚███║  ██████╔╝╚█████╔╝╚██████╔╝██║░╚███║██████╔╝
+╚═════╝░░╚════╝░╚═╝░░╚═╝╚══════╝╚══════╝╚═╝░░╚══╝  ╚═════╝░░╚════╝░░╚═════╝░╚═╝░░╚══╝╚═════╝░
+");
+    Console.WriteLine(mensagemDeBoasVindas);
+}
+
+void ExibirMenu()
+{
+    Console.WriteLine("\nDigite 1 para registrar uma banda");
+    Console.WriteLine("Digite 2 para mostrar todas as bandas");
+    Console.WriteLine("Digite 3 para avaliar uma banda");
+    Console.WriteLine("Digite 4 para exibir a média de uma banda");
+    Console.WriteLine("Digite -1 para sair");
+
+    Console.WriteLine("\n Digite sua opção: ");
+    string opcaoEscolhida = Console.ReadLine()!;
+
+    int opcaoEscolhidaNumerica = int.Parse(opcaoEscolhida);
+    switch (opcaoEscolhidaNumerica)
+    {
+        case 1: 
+            Console.WriteLine("Voce escolher a opção " + opcaoEscolhida); 
+            break;
+        case 2: 
+            Console.WriteLine("Voce escolheu a opção " + opcaoEscolhida); 
+            break;
+        case 3: 
+            Console.WriteLine("Voce escolheu a opção " + opcaoEscolhida); 
+            break;
+        case 4:
+            Console.WriteLine("Você escolheu a opção " + opcaoEscolhida);
+            break;
+        case -1:
+            Console.WriteLine("Tchau!");
+            break;
+        default: 
+            Console.WriteLine("Opção inválida"); 
+            break;
+    }
+}
+
+ExibirLogo();
+ExibirMenu();
